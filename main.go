066229e5c2ce01cd55a6e03cd71985bbe62c2ee4
fuhj_smc <fuhj_smc@si-tech.com.cn>
2022-12-28 13:44:29 +0800

    cancel终止RPC请求

--- main.go
+++ main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"context"
-	"github.com/shinemost/grpc-up-client/interceptor"
-	"log"
-	"time"
-
 	"github.com/shinemost/grpc-up-client/clients"
+	"github.com/shinemost/grpc-up-client/interceptor"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"log"
 )
 
 const (
@@ -25,10 +23,12 @@ func main() {
 	defer conn.Close()
 	//clientDeadLine := time.Now().Add(time.Second * 2)
 	//过期时间与截止时间不同，timeout每个请求单独设置，可能客户端发起一个请求会调用多个服务，那么过期时间会叠加，而截止时间deadline则是绝对时间
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	//ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	//ctx, cancel := context.WithDeadline(context.Background(), clientDeadLine)
+	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-
+	// Cancelling the RPC
+	cancel()
 	clients.AddOrder(conn, ctx)
 	//clients.UpdateOrders(conn, ctx)
 	//clients.SearchOrders(conn, ctx)
