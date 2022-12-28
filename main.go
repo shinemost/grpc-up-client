ea9666b7d4595f7988bca21ebb390b28db0a9aff
fuhj_smc <fuhj_smc@si-tech.com.cn>
2022-12-28 11:29:40 +0800

    timeout与deadline的不同

--- main.go
+++ main.go
@@ -23,9 +23,10 @@ func main() {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
-	clientDeadLine := time.Now().Add(time.Second * 2)
-	//ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	ctx, cancel := context.WithDeadline(context.Background(), clientDeadLine)
+	//clientDeadLine := time.Now().Add(time.Second * 2)
+	//过期时间与截止时间不同，timeout每个请求单独设置，可能客户端发起一个请求会调用多个服务，那么过期时间会叠加，而截止时间deadline则是绝对时间
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	//ctx, cancel := context.WithDeadline(context.Background(), clientDeadLine)
 	defer cancel()
 
 	clients.AddOrder(conn, ctx)
