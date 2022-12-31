f2b3521411469875e7f842f7f8cfe62af132f921
fuhj_smc <fuhj_smc@si-tech.com.cn>
2022-12-31 22:30:54 +0800

    gRPC负载均衡-客户端和命名解析器（name resolver)

--- main.go
+++ main.go
@@ -1,39 +1,36 @@
 package main
 
-import (
-	"context"
-	"github.com/shinemost/grpc-up-client/clients"
-	"github.com/shinemost/grpc-up-client/interceptor"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
-	"log"
-)
+import "github.com/shinemost/grpc-up-client/clients"
 
 const (
 	address = "localhost:50051"
 )
 
+//func main() {
+//	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()),
+//		grpc.WithUnaryInterceptor(interceptor.OrderUnaryClientInterceptor),
+//		grpc.WithStreamInterceptor(interceptor.ClientStreamInterceptor))
+//	if err != nil {
+//		log.Fatalf("did not connect: %v", err)
+//	}
+//	defer conn.Close()
+//	//clientDeadLine := time.Now().Add(time.Second * 2)
+//	//过期时间与截止时间不同，timeout每个请求单独设置，可能客户端发起一个请求会调用多个服务，那么过期时间会叠加，而截止时间deadline则是绝对时间
+//	//ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+//	//ctx, cancel := context.WithDeadline(context.Background(), clientDeadLine)
+//	ctx, cancel := context.WithCancel(context.Background())
+//	defer cancel()
+//	// Cancelling the RPC
+//	//cancel()
+//
+//	//RPC客户端的多路复用，多个客户端共用一个连接
+//	clients.AddOrder(conn, ctx)
+//	//clients.UpdateOrders(conn, ctx, cancel)
+//	//clients.SearchOrders(conn, ctx)
+//	//clients.ProcessOrders(conn, ctx)
+//
+//}
+
 func main() {
-	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()),
-		grpc.WithUnaryInterceptor(interceptor.OrderUnaryClientInterceptor),
-		grpc.WithStreamInterceptor(interceptor.ClientStreamInterceptor))
-	if err != nil {
-		log.Fatalf("did not connect: %v", err)
-	}
-	defer conn.Close()
-	//clientDeadLine := time.Now().Add(time.Second * 2)
-	//过期时间与截止时间不同，timeout每个请求单独设置，可能客户端发起一个请求会调用多个服务，那么过期时间会叠加，而截止时间deadline则是绝对时间
-	//ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	//ctx, cancel := context.WithDeadline(context.Background(), clientDeadLine)
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	// Cancelling the RPC
-	//cancel()
-
-	//RPC客户端的多路复用，多个客户端共用一个连接
-	clients.AddOrder(conn, ctx)
-	//clients.UpdateOrders(conn, ctx, cancel)
-	//clients.SearchOrders(conn, ctx)
-	//clients.ProcessOrders(conn, ctx)
-
+	clients.StartClient()
 }
